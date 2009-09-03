e85ad5b8d538febe6c6ea05476db0fe3c3a3752e
anantshri <anantshri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-03 02:13:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/my-profiles/trunk@151663 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myprofiles_sidebar.php
+++ myprofiles_sidebar.php
@@ -1,8 +1,8 @@
-	<div id="myprofiles_sidebar">
+	<li id="myprofiles_sidebar">
 		 <div id="myprofiles_sidebar_head">
 			<h2>My Profiles</h2>
 		 </div>
-		 <div class="clear"></div>
+	 <div class="clear"></div>
 	<div id="myprofiles_sidebar_content">
 	 
 <?php
@@ -14,4 +14,4 @@ show_myprofiles();
 			<a href="http://anantshri.info/index.php?page=wpprofiles">Grab it here</a>
 		</div>
 		<div class="clear"></div>
-	</div>
+	</li>
