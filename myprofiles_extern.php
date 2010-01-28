4dccaa2bef44c2df18b06fc7ce58f57e455cfec0
anantshri <anantshri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-28 21:46:22 +0000

    more updation : creating seperate function in extern file an currently calling from same page to check.
    git-svn-id: https://plugins.svn.wordpress.org/my-profiles/trunk@199136 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myprofiles_extern.php
+++ myprofiles_extern.php
@@ -1,4 +1,31 @@
+<html>
+<head>
+<title>My-Profiles</title>
+</head>
+<body>
 <?php
+echo "<!--Path = " . WP_CONTENT_URL . "$done-->";
+ $wpload_path = '../../../wp-load.php';
+include_once($wpload_path);
 include_once('myprofiles.php');
-widget_myprofiles();
+function profiles_view()
+{
+	echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $myprofiles_path . 'myprofiles.css" />'."\n";
 ?>
+	<div id="myprofiles_sidebar_head">		
+		<h2><?php _e('My Profiles', 'my-profiles'); ?></h2>
+	</div>
+	<div id="myprofiles_sidebar_content">
+	<?php
+	show_myprofiles();
+	?>
+	</div>
+	<div id="myprofiles_sidebar_footer">
+		<a href="http://wordpress.org/extend/plugins/my-profiles/"><?php _e('Grab it here', 'my-profiles'); ?></a>
+	</div>
+<?php
+}
+profiles_view();
+?>
+</body>
+</html>
