46167b8ecd5730291bfb2c034e6584fde452f416
anantshri <anantshri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-28 22:13:31 +0000

    checking more options.
    git-svn-id: https://plugins.svn.wordpress.org/my-profiles/trunk@199141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myprofiles_extern.php
+++ myprofiles_extern.php
@@ -4,15 +4,16 @@
 </head>
 <body>
 <?php
-echo "<!--Path = " . WP_CONTENT_URL . "$done-->";
- $wpload_path = '../../../wp-load.php';
+//echo "<!--Path = " . WP_CONTENT_URL . "$done-->";
+$wpload_path = '../../../wp-load.php';
 include_once($wpload_path);
 include_once('myprofiles.php');
-function profiles_view()
+function profiles_view($width = '100', $height = '400')
 {
 	echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $myprofiles_path . 'myprofiles.css" />'."\n";
 ?>
-	<div id="myprofiles_sidebar_head">		
+<div id="myprofiles" style='width=<?php echo $width; ?>px;height=<?php echo $height; ?>px'>
+	<div id="myprofiles_head">		
 		<h2><?php _e('My Profiles', 'my-profiles'); ?></h2>
 	</div>
 	<div id="myprofiles_sidebar_content">
@@ -23,9 +24,10 @@ function profiles_view()
 	<div id="myprofiles_sidebar_footer">
 		<a href="http://wordpress.org/extend/plugins/my-profiles/"><?php _e('Grab it here', 'my-profiles'); ?></a>
 	</div>
+</div>
 <?php
 }
-profiles_view();
+profiles_view('100','200');
 ?>
 </body>
 </html>
