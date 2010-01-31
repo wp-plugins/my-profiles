60190aeff23ef4f3cb4464d982baf61934fe296a
anantshri <anantshri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-31 00:08:14 +0000

    corrected the widget error in case of css aapplied for img taks images were not visible properly. code changed to div tag background images.
    work still left to get better anchor link.
    
    git-svn-id: https://plugins.svn.wordpress.org/my-profiles/trunk@200015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myprofiles_func.php
+++ myprofiles_func.php
@@ -37,10 +37,10 @@ function show_myprofiles($atts = 'v')
 function myprofiles_js()
 {
 global $myprofiles_path; 
+//echo $myprofiles_path;
 ?>
 function writeHTMLJS(){
 document.write("<link rel=\"stylesheet\" type=\"text\/css\" media=\"screen\" href=\"<?php echo $myprofiles_path; ?>myprofiles.css\" \/>");
-/*document.write("<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $myprofiles_path; ?>myprofiles.css" />");*/
 document.write("<div id=\"myprofiles\" style='width:" + width + "px;height:" + height + "px'>");
 document.write("	<div id=\"myprofiles_head\">		");
 document.write("		<h2>My Profiles<\/h2>");
@@ -63,8 +63,11 @@ document.write("	");
 			{
 			$url = str_replace("NAME",$list_item,$uri);
 /*document.write("<div id='myprofiles_<?php echo $count_profile; ?>'<?php if ($align=='h') then echo"style='float:left;width:45;height:45;display:block'"; ?>> <a href='<?php json_encode($url); ?>' title='<?php echo $site; ?>' target='_blank'><img border='0' src='<?php json_encode($myprofiles_path . $img); ?>' alt='<?php echo $site; ?>'\/><\/a><\/div> ");*/
+// This one is working code 
+/*document.write("<div id='myprofiles_<?php echo $count_profile; ?>' > <a href='<?php echo $url; ?>' title='<?php echo $title; ?>' target='_blank'><img border='0' src='<?php echo $myprofiles_path . $img ?>' alt='<?php echo $site; ?>'\/><\/a><\/div> ");*/
+// This works in case img tag has some css already applied to it..
 ?>
-document.write("<div id='myprofiles_<?php echo $count_profile; ?>' > <a href='<?php echo $url; ?>' title='<?php echo $title; ?>' target='_blank'><img border='0' src='<?php echo $myprofiles_path . $img ?>' alt='<?php echo $site; ?>'\/><\/a><\/div> ");
+document.write("<div id='myprofiles_<?php echo $count_profile; ?>' style='position:relative;background-image: url(<?php echo $myprofiles_path . $img ?>);background-repeat:no-repeat;'> <a href='<?php echo $url; ?>' title='<?php echo $title; ?>' target='_blank'>&nbsp;&nbsp;&nbsp;&nbsp;<\/a><\/div> ");
 <?php
 			$count_profile = $count_profile + 1;
 			}
