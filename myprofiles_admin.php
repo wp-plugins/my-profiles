07417b1bf7fa72ccfd5b49d27d50fa62b3ef1b1e
anantshri <anantshri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-09 06:15:00 +0000

    admin changes
    git-svn-id: https://plugins.svn.wordpress.org/my-profiles/trunk@203458 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- myprofiles_admin.php
+++ myprofiles_admin.php
@@ -6,17 +6,46 @@ $profileslist="";
 $count = sizeof($myprofiles);
 $inc;
 ?>
+<script>
+function test1()
+{
+	scr(document.getElementById("width").value,100);
+}
+function scr(width,height)
+{
+	var txt = "<script src='<?php echo $myprofiles_path; ?>myprofiles.php?widget=rss'>";
+//	txt = txt + "<script>var width = " + width + "var height" + height;document.getElementById("text_jscript_option").value = txt;
+	
+}
+</script>
 <h2 align="center"><?php _e('My profiles Admin panel ', 'my-profiles'); ?></h2>
 <p><?php _e('Please fill in the required details for the profiles for each service.', 'my-profiles'); ?><br />
 <font color="red"><?php _e('Currently Orkut', 'my-profiles'); ?>, <?php _e('Jyte', 'my-profiles'); ?>, <?php _e('Blogger and Indiblogger doesn', 'my-profiles'); ?>'<?php _e('t support nicknames so you have to enter their profile id', 'my-profiles'); ?>'<?php _e('s here.', 'my-profiles'); ?></font>
 
 <br />
-<a href="http://anantshri.info/index.php?page=wpprofiles"><?php _e('for more details you can check here', 'my-profiles'); ?></a>
+<a href="http://wordpress.org/extend/plugins/my-profiles/"><?php _e('for more details you can check here', 'my-profiles'); ?></a>
 </p>
 
-
+<div>
+<input type="checkbox" name="myprofiles_jscript_check"/>Check this if you wish to enable the publishing of this widget on otheer webpages<br />
+<?php
+// this needs to have a facebox popup explaining the way plugin is configured in case used as text widget.
+?>
+<b>Edit option for widget to display</b><label>Width:</label><input type="text" id="width" maxlength="4" onKeyUp="test1()" />
+<br />
+<textarea rows="6" cols="130" disabled="disabled" readonly="readonly" id="text_jscript_option">
+<script src="<?php echo $myprofiles_path; ?>myprofiles.php?widget=rss"></script>
+<script>
+	var height = 100;
+	var width = 200;
+	writeHTMLJS();
+</script>	
+</textarea>
+</div>
 <form action="options.php" method="post">
-<?php wp_nonce_field('update-options'); ?>
+<?php wp_nonce_field('update-options'); 
+//this is working on table base need to convert to div layout for ajax calls
+?>
 <table class="form-table" style="align:center;" width="100%">
 <tr>
 <td><?php _e('Name of service', 'my-profiles'); ?></td><td><?php _e('User ids', 'my-profiles'); ?></td>
@@ -54,6 +83,7 @@ foreach($myprofiles as $site=>$details)
 		<?php
 	}
 }
+// this check is to close the tr in case myprofiles are not in proper sync.
 if ($count % 3 != 0)
 {
 ?>
